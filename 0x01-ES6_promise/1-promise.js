40456f42ab06379fd6dd68533a40a7bf1493ff42
Asinmi Lukman <eemandigitalconcept@gmail.com>
2023-07-26 12:56:56 +0100

    task 2 completed

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,16 @@
+// 1-promise.js
+function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      const response = {
+        status: 200,
+        body: 'Success',
+      };
+      resolve(response);
+    } else {
+      const error = new Error('The fake API is not working currently');
+      reject(error);
+    }
+  });
+}
+export default getFullResponseFromAPI;
