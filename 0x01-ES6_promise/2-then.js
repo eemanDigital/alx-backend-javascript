de6baa4c2b9d60d609666f53bf8570bb2f822648
Asinmi Lukman <eemandigitalconcept@gmail.com>
2023-07-26 16:54:34 +0100

    task 3 completed

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,6 @@
-function handleResponseFromAPI(promise) {
-  promise
-    .then((response) => {
-      console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'success',
-      };
-    })
-    .catch((error) => {
-      console.log('Error from API');
-      return new Error(error);
-    });
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
-export default handleResponseFromAPI;
