a0db2fc71dc46110afdd7821e49fe98ece8e435f
Asinmi Lukman <eemandigitalconcept@gmail.com>
2023-07-26 11:58:03 +0100

    task one completed

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,17 @@
-export function getResponseFromAPI() {
-  return new Promise(() => {});
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Simulating an asynchronous API call with a setTimeout
+    setTimeout(() => {
+      const data = { message: 'API response data' };
+      const error = null; // Set this to a specific error object if there's an error
+
+      if (error) {
+        reject(error); // Reject the promise with an error if applicable
+      } else {
+        resolve(data); // Resolve the promise with the data
+      }
+    }, 2000); // Simulated 2-second delay
+  });
 }
+
+export default getResponseFromAPI;
