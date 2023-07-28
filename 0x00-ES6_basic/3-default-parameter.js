1bb29ed7d2349ccc36f1fa78fe9c70a593ebfa86
eemanDigital <eemandigitalconcept@gamil.com>
2023-07-28 12:22:59 +0100

    task 3 completed

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,5 +1,3 @@
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
-  if (expansion1989 && expansion2019 !== undefined) {
-    return initialNumber + expansion1989 + expansion2019;
-  }
+  return initialNumber + expansion1989 + expansion2019;
 }
