5f6381124890cd4e168e22988e0ca6eb2666a928
eemanDigital <eemandigitalconcept@gamil.com>
2023-07-28 11:52:33 +0100

    task 2 completed

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,9 +1,9 @@
-export default () => {
-    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-  
-    const self = this;
-    this.addNeighborhood = function add(newNeighborhood) {
-      self.sanFranciscoNeighborhoods.push(newNeighborhood);
-      return self.sanFranciscoNeighborhoods;
-    };
-  };
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = ((newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  });
+}
