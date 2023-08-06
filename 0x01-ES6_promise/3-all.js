ebfda232d24da6c342fb56bfed256c128e819f9e
eemanDigital <eemandigitalconcept@gamil.com>
2023-08-06 14:04:45 +0100

    task 3 completed

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((value) => {
+      console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
+    });
+}
+
+export default handleProfileSignup;
