35f2daec3cb91b7782327006bf993d1aa05db8ae
eemanDigital <eemandigitalconcept@gamil.com>
2023-08-06 14:09:55 +0100

    task 3 completed

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,8 @@ function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((value) => {
       console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
-    });
+    })
+    .catch(() => console.log('Signup system offline'));
 }
 
 export default handleProfileSignup;
