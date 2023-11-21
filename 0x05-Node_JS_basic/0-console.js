a1972c4d77a23e692a2169f81a50272236a731c2
eemanDigital <eemandigitalconcept@gamil.com>
2023-11-21 17:55:32 +0100

    project set up and tasks 0&1 completed

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg Reference the input to display.
+ */
+const displayMessage = (message) => {
+  return console.log(message);
+};
+
+module.exports = displayMessage;
